bd3179aedb95d02e1027dd89c6045804c4ecc0bb
Shredder13 <nofar.schnider@post.idc.ac.il>
2015-08-28 15:45:13 +0300

    Fixed mongoDB problems

--- actions.js
+++ actions.js
@@ -4,20 +4,27 @@ var router = express.Router();
 
 var db = require('mongoskin').db('localhost:27017/bills');
 
+var billMarkers = [];
+
 router.get('/', function(req, res){
 	// load data from DB here
-	var bills = db.bills.find()
 	res.render('index', {
 		title: 'My locations',
-		items: bills
+		items: billMarkers
 	});
 });
+
+
+router.get('/map', function(req, res){
+	res.render('map');
+});
+
 
 // post method
 //TODO: get from data base 
 router.post('/add', function(req, res){
 	var newItem = req.body.newItem;
-	db.bills.insert(newItem);
+	//db.bills.insert(newItem);
 	res.redirect('/');
 });
 
