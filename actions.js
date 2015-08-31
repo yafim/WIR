abc89bf7a2c1121a025eadfeeabe4845687c3092
yafim <ifim.vo@gmail.com>
2015-08-31 16:34:43 +0300

    Insert bill Id; about page

--- actions.js
+++ actions.js
@@ -2,23 +2,30 @@ var express = require('express');
 
 var router = express.Router();
 
-// Fake DB
+/* CREATE FAKE DB */
+var bills = [];
+
+// Each bill 
 var billMarkers = [
 	{
-		"id" : "1",
 		"currentLocation" : {"lat" : 32.3137373, "lng" : 34.8803108}
 	},
 
 	{
-		"id" : "2",
 		"currentLocation" : {"lat" : 32.069221, "lng" : 34.7734675}
 	},
 
 	{
-		"id" : "3",
 		"currentLocation" : {"lat" : 32.084508, "lng" : 34.8731652}
 	}
 ];
+
+bills.push({
+	billId: '123',
+	billMarkers : billMarkers
+});
+
+/* END OF DB*/
 
 //var db = require('mongoskin').db('localhost:27017/bills');
 
@@ -28,8 +35,20 @@ router.get('/', function(req, res){
 		title: 'My locations',
 		items: billMarkers
 	});
-	
 });
+
+// about page
+router.get('/about', function(req, res){
+	res.render('about')
+});
+
+router.get('/insertBillId', function(req, res){
+	// load data from DB here
+	res.render('insertBillId',{
+		bills : bills
+	});
+});
+
 
 // post method
 //TODO: get from data base 
@@ -39,12 +58,19 @@ router.post('/add', function(req, res){
 	var lng = req.body.lng;
 
 	billMarkers.push({
-		id: billMarkers.length + 1,
 		currentLocation: {"lat" : lat, "lng" : lng}
 	});
 
 	//var newItem = req.body.newItem;
 	//db.bills.insert(newItem);
+	res.redirect('/');
+});
+
+router.post('/billId', function(req, res){
+	var billId = req.body.billId;
+	bills.push({
+		billId: billId
+	});
 	res.redirect('/');
 });
 
