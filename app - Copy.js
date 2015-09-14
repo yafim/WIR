cf1ecd528e19ddd4797e200e1fa049354398c8c7
yafim <ifim.vo@gmail.com>
2015-09-14 14:50:14 +0300

    1232

---
+++ app - Copy.js
@@ -0,0 +1,32 @@
+//The EJS package which we have defined in package.json file is allow us to render HTML pages
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+
+
+// Configure app
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
+
+
+// Use middleware
+app.use(express.static(path.join(__dirname, 'bower_components'))); //use bootsrap
+app.use(bodyParser());
+
+// Define routes
+app.use(require('./actions'));
+
+// Start the server
+var port = Number(process.env.PORT || 3000);
+
+var server = app.listen(port, function(){
+	console.log("Server is running on port " + port);
+});
+
