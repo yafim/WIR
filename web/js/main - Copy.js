f7603489c63fe84d8c48a20d09f6b8d11b033d91
yafim <ifim.vo@gmail.com>
2015-09-29 20:52:50 +0300

    draggble button

--- web/js/main - Copy.js
+++ web/js/main - Copy.js
@@ -18,46 +18,6 @@ var app = angular.module('pageHolder', [
     }
 });
 
-app.directive('draggable', function($document) {
-  return function(scope, element, attr) {
-    var startX = 0, startY = 0, x = 780, y = -465;
-    element.css({
-     position: 'relative',
-     // border: '1px solid red',
-     // backgroundColor: 'rgb(253, 59, 115)',
-     // cursor: 'pointer',
-     // display: 'block',
-     // width: '65px',
-     left:'780px',
-     top: '-465px'
-
-
-    });
-    element.on('mousedown', function(event) {
-      // Prevent default dragging of selected content
-      event.preventDefault();
-      startX = event.screenX - x;
-      startY = event.screenY - y;
-      $document.on('mousemove', mousemove);
-      $document.on('mouseup', mouseup);
-    });
-
-    function mousemove(event) {
-      y = event.screenY - startY;
-      x = event.screenX - startX;
-      element.css({
-        top: y + 'px',
-        left:  x + 'px'
-      });
-    }
-
-    function mouseup() {
-      $document.off('mousemove', mousemove);
-      $document.off('mouseup', mouseup);
-    }
-  };
-});
-
 app.constant('facebookConfigSettings', {
     'appID' : '927002334009260',
     'routingEnabled' : true
